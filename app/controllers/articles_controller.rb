3c98a27bdc9a95a40a8d68d215997f3faa7305ea
mittani_s <hoshimi.tentai110@gmail.com>
2021-05-30 11:35:57 +0900

    - createアクションの追加 - new.html.erbのミス修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
